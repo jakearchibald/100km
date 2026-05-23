85d8ca59030836ac65f6afa294262b5f22959766
Jake Archibald <jaffathecake@gmail.com>
2026-05-23 08:27:05 +0100

    Update start-2026.ts

--- data-config/start-2026.ts
+++ data-config/start-2026.ts
@@ -1,2 +1,2 @@
-export const START_2026 = new Date('2026-05-23T08:25:00+01:00');
+export const START_2026 = new Date('2026-05-23T08:26:00+01:00');
 export const START_2026_MS = START_2026.getTime();
